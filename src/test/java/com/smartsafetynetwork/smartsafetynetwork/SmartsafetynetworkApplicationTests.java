07d4b7d17598c17acc18c8906c7a2925afce779f
isakdevelop <daishi7462@naver.com>
2024-03-29 21:03:41 +0900

    Creat - Project

---
+++ src/test/java/com/smartsafetynetwork/smartsafetynetwork/SmartsafetynetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartsafetynetwork.smartsafetynetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartsafetynetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
