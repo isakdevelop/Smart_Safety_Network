f323964c69f2b61f447f3b67e16c3b9cf7bc4236
isakdevelop <daishi7462@naver.com>
2024-04-02 21:52:11 +0900

    feat - Creating Common Components for Image Store Settings

---
+++ src/main/java/com/smartsafetynetwork/api/component/ImageStorageComponent.java
@@ -0,0 +1,22 @@
+package com.smartsafetynetwork.api.component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.UUID;
+
+@Component
+public class ImageStorageComponent {
+    @Value("${file.upload.directory}")
+    private String directory;
+
+    public String saveImage(MultipartFile image) throws IOException {
+        String fileName = UUID.randomUUID().toString() + ".jpg";
+        String imagePath = directory + "missingPerson/" + fileName;
+        Files.write(Paths.get(imagePath), image.getBytes());
+        return imagePath;
+    }
+}
