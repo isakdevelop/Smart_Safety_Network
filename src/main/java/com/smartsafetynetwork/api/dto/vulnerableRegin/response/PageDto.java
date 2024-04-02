e8aba664f21be317b4728e2fef1b4dcb615d0779
isakdevelop <daishi7462@naver.com>
2024-04-02 21:54:54 +0900

    feat - Create Common PageDto

---
+++ src/main/java/com/smartsafetynetwork/api/dto/vulnerableRegin/response/PageDto.java
@@ -0,0 +1,21 @@
+package com.smartsafetynetwork.api.dto.vulnerableRegin.response;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class PageDto {
+    private String id;
+    private String title;
+    private String content;
+    private String name;
+    private LocalDateTime creatAt;
+
+    public PageDto(String id, String title, String content, String name, LocalDateTime creatAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.name = name;
+        this.creatAt = creatAt;
+    }
+}
