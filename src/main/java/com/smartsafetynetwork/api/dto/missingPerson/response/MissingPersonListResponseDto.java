46e5fcfb6363daa80bed87dfdb205e9fad415e04
isakdevelop <daishi7462@naver.com>
2024-04-02 21:53:46 +0900

    feat - MissingPerson Detai ResponseDto

---
+++ src/main/java/com/smartsafetynetwork/api/dto/missingPerson/response/MissingPersonListResponseDto.java
@@ -0,0 +1,23 @@
+package com.smartsafetynetwork.api.dto.missingPerson.response;
+
+import lombok.Getter;
+
+@Getter
+public class MissingPersonListResponseDto {
+    private String id;
+    private String name;
+    private int age;
+    private String gender;
+    private String address;
+    private String imgPath;
+
+    public MissingPersonListResponseDto(String id, String name, int age, String gender, String address,
+                                        String imgPath) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+        this.imgPath = imgPath;
+    }
+}
