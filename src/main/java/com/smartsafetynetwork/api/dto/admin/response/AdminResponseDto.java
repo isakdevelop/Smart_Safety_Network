9db40fa18b27289393801e1107218c17297d1719
isakdevelop <daishi7462@naver.com>
2024-04-07 13:25:08 +0900

    feat - Create a joint DTO

---
+++ src/main/java/com/smartsafetynetwork/api/dto/admin/response/AdminResponseDto.java
@@ -0,0 +1,18 @@
+package com.smartsafetynetwork.api.dto.admin.response;
+
+import com.smartsafetynetwork.api.dto.ResponseDto;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+
+@SuperBuilder
+@Getter
+public class AdminResponseDto extends ResponseDto {
+    private String id;
+    private String policeNumber;
+    private String name;
+    private String password;
+    private String phone;
+    private String company;
+    private String department;
+}
