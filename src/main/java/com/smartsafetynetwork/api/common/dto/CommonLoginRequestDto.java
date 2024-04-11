cc909b809a80423a0a034d2c0c95ca008d4bf168
isakdevelop <daishi7462@naver.com>
2024-04-11 12:49:20 +0900

    refactor - remove id field

--- src/main/java/com/smartsafetynetwork/api/common/dto/CommonLoginRequestDto.java
+++ src/main/java/com/smartsafetynetwork/api/common/dto/CommonLoginRequestDto.java
@@ -1,11 +1,15 @@
 package com.smartsafetynetwork.api.common.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class CommonLoginRequestDto {
+    @NotBlank(message = "아이디가 올바르지 않습니다.")
     private String username;
+
+    @NotBlank(message = "패스워드가 올바르지 않습니다.")
     private String password;
 }
--- src/main/java/com/smartsafetynetwork/api/common/dto/CommonLoginResponseDto.java
+++ src/main/java/com/smartsafetynetwork/api/common/dto/CommonLoginResponseDto.java
@@ -1,18 +1,18 @@
 package com.smartsafetynetwork.api.common.dto;
 
-import com.smartsafetynetwork.api.dto.ResponseDto;
+import com.smartsafetynetwork.api.enums.Role;
 import lombok.Getter;
 
 @Getter
 public class CommonLoginResponseDto extends ResponseDto {
     private String jwt;
     private int expirationTime;
-    private String id;
+    private String role;
 
-    public CommonLoginResponseDto(int status, String message, String jwt, int expirationTime, String id) {
+    public CommonLoginResponseDto(int status, String message, String jwt, int expirationTime, Role role) {
         super(status, message);
         this.jwt = jwt;
         this.expirationTime = expirationTime;
-        this.id = id;
+        this.role = String.valueOf(role);
     }
 }
