4ff1e5a8676f4974e142916e017dddffaba35c03
isakdevelop <daishi7462@naver.com>
2024-04-11 12:57:42 +0900

    refactor - Change to component to use bean

--- src/main/java/com/smartsafetynetwork/api/component/RandomPasswordGenerator.java
+++ src/main/java/com/smartsafetynetwork/api/component/RandomPasswordGenerator.java
@@ -1,7 +1,9 @@
-package com.smartsafetynetwork.api.common;
+package com.smartsafetynetwork.api.component;
 
 import java.security.SecureRandom;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RandomPasswordGenerator {
     private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
     private static final String CHAR_UPPER = CHAR_LOWER.toUpperCase();
@@ -11,7 +13,7 @@ public class RandomPasswordGenerator {
     private static final String PASSWORD_ALLOW_BASE = CHAR_LOWER + CHAR_UPPER + NUMBER + SPECIAL_CHARS;
     private static final SecureRandom random = new SecureRandom();
 
-    public static String generateRandomPassword() {
+    public String generateRandomPassword() {
         StringBuilder sb = new StringBuilder(12);
         for (int i = 0; i < 12; i++) {
             int rndCharAt = random.nextInt(PASSWORD_ALLOW_BASE.length());
