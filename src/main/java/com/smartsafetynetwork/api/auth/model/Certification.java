2aaf66dd43595f1eaef56ee4be2803582e80135f
isakdevelop <daishi7462@naver.com>
2024-05-20 19:35:57 +0900

    refactor - change database Mysql to redis

--- src/main/java/com/smartsafetynetwork/api/auth/model/Certification.java
+++ src/main/java/com/smartsafetynetwork/api/auth/model/Certification.java
@@ -1,28 +1,15 @@
 package com.smartsafetynetwork.api.auth.model;
 
-import com.smartsafetynetwork.api.common.model.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
-@Entity(name = "certification")
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
 @Builder
-public class Certification extends BaseEntity {
+@RedisHash(value = "email", timeToLive = 300)
+public class Certification {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
     private String email;
     private String certificationNumber;
 }
--- src/main/java/com/smartsafetynetwork/api/auth/repository/CertificationRepository.java
+++ src/main/java/com/smartsafetynetwork/api/auth/repository/CertificationRepository.java
@@ -1,10 +1,9 @@
 package com.smartsafetynetwork.api.auth.repository;
 
 import com.smartsafetynetwork.api.auth.model.Certification;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CertificationRepository extends JpaRepository<Certification, String> {
-    boolean existsByCertificationNumber(String password);
+public interface CertificationRepository extends CrudRepository<Certification, String>  {
 }
