9210bd67750e0a75184831b9e114dc2fa7711cb9
isakdevelop <daishi7462@naver.com>
2024-05-20 20:15:27 +0900

    feat - add POLICE Role

--- src/main/java/com/smartsafetynetwork/api/common/enums/Role.java
+++ src/main/java/com/smartsafetynetwork/api/common/enums/Role.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 public enum Role {
     ROLE_USER("ROLE_USER"),
     ROLE_ADMIN("ROLE_ADMIN"),
-    ROLE_DRIVER("ROLE_DRIVER")
+    ROLE_DRIVER("ROLE_DRIVER"),
+    ROLE_POLICE("ROLE_POLICE")
     ;
 
     private final String role;
