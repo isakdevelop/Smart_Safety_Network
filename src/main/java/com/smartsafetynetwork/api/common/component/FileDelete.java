50417f42d6bb2120227df6eff1ecb1fa374ff39c
isakdevelop <daishi7462@naver.com>
2024-05-20 20:12:32 +0900

    feat - add fileDelete function

---
+++ src/main/java/com/smartsafetynetwork/api/common/component/FileDelete.java
@@ -0,0 +1,15 @@
+package com.smartsafetynetwork.api.common.component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileDelete {
+    public void deleteFile(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        Files.deleteIfExists(path);
+    }
+}
